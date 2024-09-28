e84ddf1ecac9bc99c7ff3e1e3260f4a0377e4939
Hussain <hussain@Hussains-MacBook-Pro.local>
2024-09-28 10:44:18 +0530

    Print Name n times using recursion

--- Step-1/Lec-5/print_name_recursion.py
+++ Step-1/Lec-5/print_name_recursion.py
@@ -1,12 +1,17 @@
 """Program to print name n times using recursion."""
 
+
 class Solution:
+
     def __init__(self):
         pass
+
     def printName(self, n):
         if n > 0:
-            self.printName(n-1)
+            self.printName(n - 1)
             print("Hussain", end="\t")
 
+
 solution = Solution()
+
 solution.printName(5)
